cddce0c429d542aa8f21ea894dd146c367e3f2e7
solji <thfwl0094@github.com>
2022-09-05 01:33:23 +0900

    0905 commit

---
+++ java_tutorials/src/easyJava_0902/Main.java
@@ -0,0 +1,14 @@
+package easyJava_0902;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int year = scan.nextInt();
+		System.out.println(year-543);
+	}
+
+}
